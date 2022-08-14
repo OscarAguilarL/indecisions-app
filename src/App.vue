bbef270bd8c01f7158f73ae168d88842734020a1
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-08-14 17:33:42 -0500

    fin seccion 4

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <counter :start="10" />
+  <counter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Counter,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>{{ count }} <sup>2</sup> = {{ getSquare }}</p>
+
+  <button @click="incrementCount">+1</button>
+  <button @click="decrementCount">-1</button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 1,
+    },
+  },
+  data() {
+    return {
+      count: this.start || 0,
+    };
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    },
+    decrementCount() {
+      this.count--;
+    },
+  },
+  computed: {
+    getSquare() {
+      return this.count * this.count;
+    },
+    customTitle() {
+      return this.title || "Counter";
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #64b687;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+}
+</style>
